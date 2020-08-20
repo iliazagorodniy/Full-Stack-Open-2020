50612d9f2b4023ebd279a0905b3933e2a36dee1a
Ilia <izagorodn@gmail.com>
2020-08-20 18:55:37 +0300

    started paragraph C of part 1

--- part_1/courseinfo/src/index.js
+++ part_1/courseinfo/src/index.js
@@ -1,20 +1,3 @@
-// Exercise 2
-
-// Refactor the Content component so that it does not render any names of parts
-// or their number of exercises by itself.
-// Instead it only renders three Part components of which
-// each renders the name and number of exercises of one part.
-//
-//     const Content = ... {
-//         return (
-//             <div>
-//                 <Part .../>
-//                 <Part .../>
-//                 <Part .../>
-//             </div>
-//         )
-//     }
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
